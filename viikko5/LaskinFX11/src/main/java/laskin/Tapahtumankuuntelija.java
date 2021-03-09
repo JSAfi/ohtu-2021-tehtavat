0a121dd8ad356e0004759533396bd345cbed9b2d
JSAfi <jaakko.m.salmela@gmail.com>
2021-03-09 18:58:08 +0200

    Summaa -komento toteutettu, handle muutettu

--- viikko5/LaskinFX11/src/main/java/laskin/Tapahtumankuuntelija.java
+++ viikko5/LaskinFX11/src/main/java/laskin/Tapahtumankuuntelija.java
@@ -5,6 +5,9 @@ import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Tapahtumankuuntelija implements EventHandler {
     private TextField tuloskentta; 
     private TextField syotekentta; 
@@ -13,6 +16,8 @@ public class Tapahtumankuuntelija implements EventHandler {
     private Button nollaa;
     private Button undo;
     private Sovelluslogiikka sovellus;
+    private Map<Button, Komento> komennot;
+    private Komento edellinen = null;
 
     public Tapahtumankuuntelija(TextField tuloskentta, TextField syotekentta, Button plus, Button miinus, Button nollaa, Button undo) {
         this.tuloskentta = tuloskentta;
@@ -22,8 +27,21 @@ public class Tapahtumankuuntelija implements EventHandler {
         this.nollaa = nollaa;
         this.undo = undo;
         this.sovellus = new Sovelluslogiikka();
+        this.komennot = new HashMap<>();
+        this.komennot.put(plus, new Summa(tuloskentta, syotekentta, plus, miinus, nollaa, undo, sovellus));
     }
-    
+    @Override
+    public void handle(Event event) {
+        if( event.getTarget() != undo) {
+            Komento komento = this.komennot.get((Button)event.getTarget());
+            komento.suorita();
+            this.edellinen = komento;
+        } else {
+            this.edellinen.peru();
+            this.edellinen = null;
+        }
+    }
+    /*
     @Override
     public void handle(Event event) {
         int arvo = 0;
@@ -55,5 +73,5 @@ public class Tapahtumankuuntelija implements EventHandler {
         }
         undo.disableProperty().set(false);
     }
-
+    */
 }
